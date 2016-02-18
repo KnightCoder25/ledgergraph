d7c1e922564344d56328798bbf48f0e6b9f2d89c
rohit <rohit.singh@infibeam.net>
2016-02-18 19:14:58 +0530

    Fix XAxis

--- app/src/main/java/android/iris/graph/LedgerGraph.java
+++ app/src/main/java/android/iris/graph/LedgerGraph.java
@@ -20,15 +20,24 @@ public class LedgerGraph extends View {
     private static String TAG = "LedgerGraph";
     private float maxValue = 0;
     private int percentageWitdh = 20;
+    // In Dp
+    private int strokeWidth = 1;
+    private int paddingInDp = 8;
 
-    private float oldLeft = 0;
 
     private Paint paintCredit;
     private Paint paintDebit;
+    private Paint paintLines;
+
+    private float xAxis=0;
+
+    private int paddingInPixels;
 
 
+    private ArrayList<Float> barCenters = new ArrayList<>();
 
-    private ArrayList<BarData> datas= new ArrayList<>();
+
+    private ArrayList<BarData> datas = new ArrayList<>();
 
     public LedgerGraph(Context context) {
         super(context);
@@ -47,35 +56,46 @@ public class LedgerGraph extends View {
     }
 
 
-    public void setData(ArrayList<BarData> newDatas){
-       this.datas = newDatas;
+    public void setData(ArrayList<BarData> newDatas) {
+        this.datas = newDatas;
     }
 
 
-    public void draw(){
+    public void draw() {
+
+
+        paddingInPixels = (int) getPixels(paddingInDp);
+
+        Log.v(TAG, "paddingInPixels " + paddingInPixels);
+
 
         paintCredit = new Paint();
-        paintCredit.setColor(Color.BLACK);
+        paintCredit.setColor(Color.CYAN);
 
         paintDebit = new Paint();
         paintDebit.setColor(Color.BLUE);
 
-        if (datas.isEmpty()){
-            Log.v(TAG,"No data set");
+        paintLines = new Paint();
+        paintLines.setColor(Color.GRAY);
+        paintLines.setStrokeWidth(getPixels(strokeWidth));
+        paintLines.setStyle(Paint.Style.STROKE);
+
+
+        if (datas.isEmpty()) {
+            Log.v(TAG, "No data set");
             return;
         }
-        for (BarData bar:datas){
-            float currentValue = bar.getCredit()+bar.getDebit();
-            if (maxValue <=currentValue){
-                maxValue =currentValue;
+        for (BarData bar : datas) {
+            float currentValue = bar.getCredit() + bar.getDebit();
+            if (maxValue <= currentValue) {
+                maxValue = currentValue;
             }
         }
-        Log.v(TAG, "MAX RANGE: "+ maxValue);
+        Log.v(TAG, "MAX RANGE: " + maxValue);
 
         invalidate();
 
     }
-
 
 
     @Override
@@ -84,52 +104,77 @@ public class LedgerGraph extends View {
         int width = canvas.getWidth();
         int height = canvas.getHeight();
 
-
-        if (datas.isEmpty()){
+        if (datas.isEmpty()) {
             return;
         }
-        Log.v(TAG,"onDraw width: "+width+" height: "+height);
+        Log.v(TAG, "onDraw width: " + width + " height: " + height);
+
+         float unitHeight = height/100f;
 
 
+        int topPadding = (int)(5*unitHeight);
 
 
-         canvas.drawBitmap(drawBars(width,height),0,0,new Paint());
+        canvas.drawBitmap(drawBars(width, (int)(80*unitHeight), 0)
+                , 0, topPadding, new Paint());
+
+
+        // X Axis
+        canvas.drawLine(0f, xAxis+topPadding, width, xAxis+topPadding, paintLines);
+        // Y Axis
+        canvas.drawLine((float) paddingInPixels, 0f, (float) paddingInPixels, height, paintLines);
 
     }
 
-    private Bitmap drawBars(int width, int height){
+    private Bitmap drawBars(int width, int height, float left) {
+
+
+        Log.v(TAG,"drawBars Width: "+width+" height:"+height+" left:"+left);
 
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 
-        Canvas canvas = new Canvas(bitmap);
-        float widthWithSpace = width/datas.size();
+        float oldLeft = left;
 
-        float barSpace = widthWithSpace*(percentageWitdh/100f);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.WHITE);
+
+        float widthWithSpace = width / datas.size();
+
+        float barSpace = widthWithSpace * (percentageWitdh / 100f);
         float barWidth = widthWithSpace - barSpace;
 
-        for (BarData barData: datas){
 
-            float left = oldLeft+barSpace;
-            float right = left+barWidth;
-            float creditBottom = height/2;
+        barCenters.clear();
 
-            float unit = height/maxValue;
+
+        for (BarData barData : datas) {
+
+            float barLeft = oldLeft + barSpace;
+            float right = barLeft + barWidth;
+            float creditBottom = height / 2;
+
+            // Calculate the centres
+            barCenters.add(right/2f);
+
+
+            float unit = height / maxValue;
 
             float creditHeight = unit * barData.getCredit();
             float creditTop = creditBottom - creditHeight;
 
             float debitHeight = unit * barData.getDebit();
             float debitTop = creditBottom;
-            float debitBottom = debitTop+debitHeight;
+            float debitBottom = debitTop + debitHeight;
 
+            xAxis = creditBottom;
 
+            Log.v(TAG,"draw bar barLeft:"
+                    +barLeft+" creditTop "+creditTop+" right:"+right+" creditBottom: "+creditBottom);
 
-         //   Log.v(TAG,"onDraw left: "+left+" right: "+right+" top: "+top+" bottom: "+bottomCredit);
             // Draw credit
-            canvas.drawRect(left,creditTop,right,creditBottom,paintCredit);
+            canvas.drawRect(barLeft, creditTop, right, creditBottom, paintCredit);
             // Draw debit
-            canvas.drawRect(left,debitTop,right,debitBottom,paintDebit);
-
+            canvas.drawRect(barLeft, debitTop, right, debitBottom, paintDebit);
 
             oldLeft = right;
 
@@ -139,9 +184,19 @@ public class LedgerGraph extends View {
     }
 
 
+    public float getPixels(int dp) {
 
+        //Resources r = boardContext.getResources();
+        //float px = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpis, r.getDisplayMetrics());
 
+        final float scale = this.getContext().getResources().getDisplayMetrics().density;
+        int px = (int) (dp * scale + 0.5f);
 
+        Log.v(TAG, "getPixels: " + strokeWidth);
+
+        return px;
+
+    }
 
 
 }
